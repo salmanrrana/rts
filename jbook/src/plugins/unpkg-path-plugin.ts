809afe430ac55e47649f436bf00bfa7a80e545eb
Salman Rana <salman.r.rana@gmail.com>
2021-02-28 14:46:34 -0600

    include caching layer with localforage

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name: 'filecahce'
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -42,16 +47,28 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        // check to see if we have already fetched the file/pkg
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        // if it is, return immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
 
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
+
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
 };
-
-// unpkg.com/tiny-test-pkg
